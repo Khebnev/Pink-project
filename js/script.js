804445ca45366d7c243506344312e5dcc1677f18
Anton <khebnev.a.v@gmail.com>
2020-05-19 17:17:35 +0300

    Перенёс файлы со старого репозитория

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var menu = document.querySelector(".navigation__toggle");
+
+menu.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    console.log("Клик по бургер-меню");
+
+});
+
+
+
+
